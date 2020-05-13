94615a4fbb0ba0f1c1035cd32a38b92739aee39a
Witcent <64896426+Witcent@users.noreply.github.com>
2020-05-13 15:50:44 +0300

    1-st homework

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
+let algebra = prompt('Оценка по алгебре: ');
+let geography = prompt('Оценка по георграфии: ');
+let physics = prompt('Оценка по физике: ');
+let average = algebra + geography + physics / 3;
+alert(average);
 
+let myName = promt('Ваше имя: ');
+фдуке('Привет, мир! Меня зовут ' + myName);
